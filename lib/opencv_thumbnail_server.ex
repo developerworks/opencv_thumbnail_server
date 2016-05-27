35226be3e4281d731f82fbd2644f88c79a0ab38a
developerworks <developerworks@163.com>
2016-05-27 18:41:04 +0800

    Add set_cookie when application start

--- lib/opencv_thumbnail_server.ex
+++ lib/opencv_thumbnail_server.ex
@@ -3,6 +3,7 @@ defmodule OpencvThumbnailServer do
   use Application
   def start(_type, _args) do
     Logger.info "Start opencv thumbnail server"
+    :erlang.set_cookie(node, :"&!CQMYY$_123+_-*%#ELIXIR.APPLICATION")
     OpencvThumbnailServer.Supervisor.start_link()
   end
 end
