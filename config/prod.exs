2d402a06ada62a359094e06173124cf98e060b23
developerworks <developerworks@163.com>
2016-06-03 15:34:11 +0800

    fix: could not start

--- config/prod.exs
+++ config/prod.exs
@@ -1,34 +1,34 @@
 use Mix.Config
 
-config :kernel, distributed: [{:opencv_thumbnail_server, 5000, [
-  :"node1@192.168.212.45",
-  :"node2@192.168.212.45",
-  :"node3@192.168.212.45",
-  :"node4@192.168.212.45",
-  :"node5@192.168.212.45"
-]}]
+# config :kernel, distributed: [{:opencv_thumbnail_server, 5000, [
+#   :"node1@192.168.212.45",
+#   :"node2@192.168.212.45",
+#   :"node3@192.168.212.45",
+#   :"node4@192.168.212.45",
+#   :"node5@192.168.212.45"
+# ]}]
 
-config :kernel, inet_dist_listen_min: 9100
+# config :kernel, inet_dist_listen_min: 9100
 
-config :kernel, inet_dist_listen_max: 9105
+# config :kernel, inet_dist_listen_max: 9105
 
-config :kernel, sync_nodes_mandatory: [
-  :"node2@192.168.212.45",
-  :"node3@192.168.212.45"
-]
-config :kernel, sync_nodes_optional: [
-  :"node4@192.168.212.45",
-  :"node5@192.168.212.45"
-]
+# config :kernel, sync_nodes_mandatory: [
+#   :"node2@192.168.212.45",
+#   :"node3@192.168.212.45"
+# ]
+# config :kernel, sync_nodes_optional: [
+#   :"node4@192.168.212.45",
+#   :"node5@192.168.212.45"
+# ]
 
-config :kernel, sync_nodes_timeout: 10000
+# config :kernel, sync_nodes_timeout: 10000
 
-config :kernel, global_groups: [{:opencv_thumbnail_server_group, [
-  :"node1@192.168.212.45",
-  :"node2@192.168.212.45",
-  :"node3@192.168.212.45"
-]}]
+# config :kernel, global_groups: [{:opencv_thumbnail_server_group, [
+#   :"node1@192.168.212.45",
+#   :"node2@192.168.212.45",
+#   :"node3@192.168.212.45"
+# ]}]
 
-config :syn, registry_process_exit_callback: [
-  OpencvThumbnailServer.ExitCallback, :callback_on_process_exit
-]
+# config :syn, registry_process_exit_callback: [
+#   OpencvThumbnailServer.ExitCallback, :callback_on_process_exit
+# ]
