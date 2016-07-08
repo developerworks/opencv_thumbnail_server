797d18e733947ab1ef0c22c2a443cd0a9fe44018
developerworks <developerworks@163.com>
2016-07-08 17:39:18 +0800

    bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule OpencvThumbnailServer.Mixfile do
 
   def project do
     [app: :opencv_thumbnail_server,
-     version: "0.0.2",
+     version: "0.0.4",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
